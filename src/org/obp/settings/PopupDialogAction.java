5a2be2caf1a1127c5d01e54ff585094b44c08f4d
Vladimir Kozhaev <vkozhaev@gmail.com>
2021-03-26 22:47:01 +0300

    Add an example query

--- src/org/obp/settings/PopupDialogAction.java
+++ src/org/obp/settings/PopupDialogAction.java
@@ -3,12 +3,20 @@ package org.obp.settings;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.pom.Navigatable;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import java.io.IOException;
 
 public class PopupDialogAction extends AnAction {
 
@@ -40,12 +48,26 @@ public class PopupDialogAction extends AnAction {
         PushCodeDialog pushCodeDialog = new PushCodeDialog(selectedText);
 
         if (pushCodeDialog.showAndGet()) {
-            System.out.println(pushCodeDialog.getFunctionNameText());
+
+
+
+            try {
+                Unirest.setTimeouts(0, 0);
+                HttpResponse<String> response = null;
+                response = Unirest.get("https://test.openbankproject.com/obp/v4.0.0/users/current")
+                        .header("Authorization", "DirectLogintoken=eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.xe95UT3ZvjUC-BXjtk6rGQuUeJfyyIS1Ha5XsUaRdr0")
+                        .header("Content-Type", "application/json")
+                        .header("Cookie", "JSESSIONID=node04oiowjti87aa3z7iksnpkg619930.node0")
+                        .asString();
+
+
+                Messages.showMessageDialog(currentProject, "response:"+response.getBody(), dlgTitle, Messages.getInformationIcon());
+            } catch (UnirestException e) {
+                Messages.showMessageDialog(currentProject, e.getStackTrace().toString(), dlgTitle, Messages.getInformationIcon());
+            }
         }
 
-/*
-        Messages.showMessageDialog(currentProject, "Host Version is:" + instance.getHostVersion() +
-                "\nSelected Text:" + selectedText, dlgTitle, Messages.getInformationIcon());*/
+
     }
 
 
