2e95320d2d70f0f7a25dfa8a6d0864a6decb7554
Vladimir Kozhaev <vkozhaev@gmail.com>
2021-03-29 22:28:17 +0300

    Commit attempts to fix the method body encoding

--- src/org/obp/settings/PopupDialogAction.java
+++ src/org/obp/settings/PopupDialogAction.java
@@ -1,15 +1,18 @@
 package org.obp.settings;
 
+
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.pom.Navigatable;
 
+
+import org.apache.commons.httpclient.URIException;
+import org.apache.commons.httpclient.util.URIUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -47,20 +50,29 @@ public class PopupDialogAction extends AnAction {
         Caret primaryCaret = editor.getCaretModel().getPrimaryCaret();
 
         String selectedText = primaryCaret.getSelectedText();
-        PushCodeDialog pushCodeDialog = new PushCodeDialog(selectedText);
+        String escapeCode= null;
+        try {
+            escapeCode = URIUtil.encodeQuery("def hello = println(\"Hello, world!\")");
+        } catch (URIException e) {
+            e.printStackTrace();
+        }
+
+        PushCodeDialog pushCodeDialog = new PushCodeDialog(escapeCode);
 
         if (pushCodeDialog.showAndGet()) {
 
 
             try {
                 JSONObject json = new JSONObject();
-                json.put("method_name", "checkExternalUserExists").put("method_body", "hello()");
+                json.put("method_name", "checkExternalUserExists").put("method_body", "%20%20%20%20%20%20Future.successful%28%0A%20%20%20%20%20%20%20%20Full%28%28BankCommons%28%0A%20%20%20%20%20%20%20%20%20%20BankId%28%22Hello%20bank%20id%22%29%2C%0A%20%20%20%20%20%20%20%20%20%20%221%22%2C%0A%20%20%20%20%20%20%20%20%20%20%221%22%2C%0A%20%20%20%20%20%20%20%20%20%20%221%22%2C%0A%20%20%20%20%20%20%20%20%20%20%221%22%2C%0A%20%20%20%20%20%20%20%20%20%20%221%22%2C%0A%20%20%20%20%20%20%20%20%20%20%221%22%2C%0A%20%20%20%20%20%20%20%20%20%20%221%22%2C%0A%20%20%20%20%20%20%20%20%20%20%228%22%0A%20%20%20%20%20%20%20%20%29%2C%20None%29%29%0A%20%20%20%20%20%20%29");
+
                 Unirest.setTimeouts(0, 0);
+
                 HttpResponse<String> response = Unirest.put("https://test.openbankproject.com/obp/v4.0.0/management/connector-methods/ca34ff25-25f0-4c62-be2d-b3627e96d356")
                         .header("Authorization", "DirectLogintoken=eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.xe95UT3ZvjUC-BXjtk6rGQuUeJfyyIS1Ha5XsUaRdr0")
                         .header("Content-Type", "application/json")
                         .header("Cookie", "JSESSIONID=node0umackg1zhun41xye364x7ghtl10069.node0")
-                        .body(json.put("method_name", "getBank").put("method_body", "println(\"Hello, world\")").toString())
+                        .body(json.put("method_name", "getBank").put("method_body", escapeCode).toString())
                         .asString();
 
 
