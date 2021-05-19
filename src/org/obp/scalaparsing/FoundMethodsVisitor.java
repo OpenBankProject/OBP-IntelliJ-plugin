f3d66d1e7ceac51784092c9839c8ceffac9ace87
Vladimir Kozhaev <vkozhaev@gmail.com>
2021-05-19 23:58:58 +0300

    Refactoring and add the icon

--- src/org/obp/scalaparsing/FoundMethodsVisitor.java
+++ src/org/obp/scalaparsing/FoundMethodsVisitor.java
@@ -1,19 +1,9 @@
 package org.obp.scalaparsing;
 
 import obp.settings.antlrgenerated.ScalaBaseVisitor;
-import obp.settings.antlrgenerated.ScalaLexer;
-import obp.settings.antlrgenerated.ScalaParser;
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.atn.PredictionMode;
-import org.antlr.v4.runtime.misc.Interval;
-import org.antlr.v4.runtime.tree.ParseTree;
-import org.codehaus.plexus.util.StringInputStream;
-import org.jetbrains.annotations.NotNull;
 
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -21,29 +11,8 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class FoundMethodsVisitor extends ScalaBaseVisitor<ScalaCode> {
-    public List<ScalaFunction> getFunctions() {
-        return functions;
-    }
-    private ANTLRInputStream antlrInputStream;
 
-    public FoundMethodsVisitor(ANTLRInputStream antlrInputStream) {
-        this.antlrInputStream = antlrInputStream;
-    }
 
-    List<ScalaFunction> functions = new ArrayList<ScalaFunction>();
-    @Override
-    public ScalaCode visitFunDef(ScalaParser.FunDefContext ctx) {
-
-        if (ctx.funSig() != null) {
-            ScalaFunction scalaFunction = getScalaFunction(ctx);
-            functions.add(scalaFunction);
-            return scalaFunction;
-        } else if (ctx.constrExpr() != null) {
-            return visit(ctx.constrExpr());
-        } else {
-            return visit(ctx.constrBlock());
-        }
-    }
 
     public static List<ScalaFunction> parseScalaFunction(String programText) throws IOException {
         Pattern overrideDefPattern=Pattern.compile("((\\s*)(override)(\\s+)def(\\s+))|((\\s*)def(\\s+))");
@@ -76,22 +45,5 @@ public class FoundMethodsVisitor extends ScalaBaseVisitor<ScalaCode> {
 
     }
 
-    @NotNull
-    private ScalaFunction getScalaFunction(ScalaParser.FunDefContext ctx) {
-        if (ctx.expr() != null) {
-            ScalaFunction scalaFunction = new ScalaFunction( retrieveCtxText(ctx.expr()),ctx.funSig().Id().getText());
-            return scalaFunction;
-        } else {
-            ScalaFunction scalaFunction = new ScalaFunction( retrieveCtxText(ctx.block()),ctx.funSig().Id().getText());
-            return scalaFunction;
-        }
-    }
 
-    private String retrieveCtxText(ParserRuleContext ctx){
-        int a = ctx.start.getStartIndex();
-        int b = ctx.stop.getStopIndex();
-
-        Interval interval = new Interval(a,b);
-        return antlrInputStream.getText(interval);
-    }
 }
